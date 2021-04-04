40a72ea5eb3cf352547163d3eda9a6c394b75d4b
arpad.bajzath <arpad.bajzath@gmail.com>
2021-04-04 16:21:06 +0200

    add some content

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,10 @@
 <template>
   <div id="app">
-
     <div class="min-w-screen min-h-screen bg-gray-800 flex items-center justify-center px-5 py-5">
       <div class="w-full mx-auto rounded-xl bg-gray-100 shadow-lg p-10 text-gray-800 relative overflow-hidden min-w-80 max-w-3xl">
         <h1 class="font-semibold text-gray-800 dark:text-gray-200 sm:text-lg mb-5">Facebook adatszivárgás ellenörző</h1>
-        <p class="mb-5">Ezen a felületen biztonságosan ellenőrizheted, hogy a felhasználói fiókod érintett-e a korábbi adatszivárgásokban.
-          <small>
-            <br><em>Disclaimer:</em> ez az adatbázis az interneten fellelhető,
-            <a href="https://raketa.hu/legalabb-377-ezer-magyar-facebookozo-adatait-tettek-kozze-nyilvanosan-a-neten" target="_blank" class="underline">ellopott</a>
-            377.000 magyar adatainak <strong>butított</strong> változata, mely semmilyen személyes adatot nem tartalmaz.
-            Az oldal kizárólagos célja, hogy a felhasználóknak lehetősége legyen ellenőrizni érintett-e az adatszivárgásban.</small>
+        <p class="mb-5">
+          Ezen a felületen biztonságosan ellenőrizheted, hogy a felhasználói fiókod érintett-e a korábbi adatszivárgásokban.
         </p>
         <p class="mb-5">Keresni a <a class="underline" href="https://findmyfbid.in/" target="_blank">felhasználói azonosítódra</a>, vagy a telefonszámodra tudsz, a következő formában: 361234567</p>
         <div class="relative mt-1">
@@ -27,8 +22,19 @@
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="#ccc" d="M9.145 18.29c-5.042 0-9.145-4.102-9.145-9.145s4.103-9.145 9.145-9.145 9.145 4.103 9.145 9.145-4.102 9.145-9.145 9.145zm0-15.167c-3.321 0-6.022 2.702-6.022 6.022s2.702 6.022 6.022 6.022 6.023-2.702 6.023-6.022-2.702-6.022-6.023-6.022zm9.263 12.443c-.817 1.176-1.852 2.188-3.046 2.981l5.452 5.453 3.014-3.013-5.42-5.421z"/></svg>
           </button>
         </div>
-        <div class="py-3 px-2 mt-4 mb-0 bg-green-300 text-green-800 rounded border border-green-600">Nem szerepel</div>
-        <div class="py-3 px-2 my-2 bg-red-300 text-red-800 rounded border border-red-600">Szerepel. <a class="underline" href="#">Mi a teendő?</a></div>
+        <div
+          v-if="isSubmitted && !isHit"
+          class="py-3 px-2 mt-4 mb-0 bg-green-300 text-green-800 rounded border border-green-600"
+        >Nem szerepel</div>
+        <div
+          v-if="isSubmitted && isHit"
+          class="py-3 px-2 my-2 bg-red-300 text-red-800 rounded border border-red-600 mt-4"><strong>Szerepel!</strong> Mi a teendő? Érdemes jelszót cserélj minden általad fontosabbnak tartott oldalon, ügyelj rá, hogy megfelelő erősségű jelszót használj és ahol lehet a kétlépcsős azonosítást használd!</div>
+
+        <p class="mt-4"><small>
+          <em>Disclaimer:</em> ez az adatbázis az interneten fellelhető,
+          <a href="https://raketa.hu/legalabb-377-ezer-magyar-facebookozo-adatait-tettek-kozze-nyilvanosan-a-neten" target="_blank" class="underline">ellopott</a>
+          377.000 magyar adatainak <strong>butított</strong> változata, mely semmilyen személyes adatot nem tartalmaz.
+          Az oldal kizárólagos célja, hogy a felhasználóknak lehetősége legyen ellenőrizni érintettek-e az adatszivárgásban.</small></p>
       </div>
     </div>
 
@@ -55,7 +61,7 @@ export default {
 
   data () {
     return {
-      queryText: 'bf974226d2c47745ce5905fa963d8d4eb062053bbbd1367d48ca7d74294dc3ba',
+      queryText: '',
       isSubmitted: false,
       isLoading: true,
       isHit: false,
@@ -65,21 +71,20 @@ export default {
 
   created () {
     firebase.initializeApp(firebaseConfig);
-
-    sha256('')
+    firebase.setLogLevel('debug')
   },
 
   methods: {
     async check () {
+      this.isSubmitted = false;
       const db = firebase.firestore();
       const users = db.collection('users')
-      const phone =  users.where('phone', '==', (this.queryText)).get()
-      const id = users.where('id', '==', (this.queryText)).get()
-
-      users.get().then(console.log)
+      const phone =  users.where('phone', '==', sha256(this.queryText)).get()
+      const id = users.where('id', '==', sha256(this.queryText)).get()
 
       const [phoneSnapshot, idSnapshot] = await Promise.all([phone, id])
 
+      this.isSubmitted = true;
       const phoneSize = phoneSnapshot.size
       const idSize = idSnapshot.size
 
